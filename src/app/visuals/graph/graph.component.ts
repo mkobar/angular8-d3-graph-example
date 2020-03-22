23f0cfc7778f0516c723081f553a9789db40ac73
mkobar <mike@exlorev.com>
2020-03-22 19:45:15 -0400

    move html to own file

--- src/app/visuals/graph/graph.component.ts
+++ src/app/visuals/graph/graph.component.ts
@@ -4,15 +4,7 @@ import { D3Service, ForceDirectedGraph, Node } from '../../d3';
 @Component({
   selector: 'graph',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `
-    <svg #svg [attr.width]="_options.width" [attr.height]="_options.height">
-	<!--<g [zoomableOf]="svg">-->
-        <g [linkVisual]="link" *ngFor="let link of links"></g>
-        <g [nodeVisual]="node" *ngFor="let node of nodes"
-           [draggableNode]="node" [draggableInGraph]="graph"></g>
-        <!--</g>-->
-    </svg>
-  `,
+  templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent implements OnInit, AfterViewInit {
@@ -20,7 +12,7 @@ export class GraphComponent implements OnInit, AfterViewInit {
   @Input('links') links;
   graph: ForceDirectedGraph;
   //private _options: { width, height } = { width: 800, height: 600 };
-  _options: { width, height } = { width: 800, height: 600 };
+  _options: { width, height } = { width: 666, height: 150 };
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
@@ -28,16 +20,25 @@ export class GraphComponent implements OnInit, AfterViewInit {
   }
 
 
-  constructor(private d3Service: D3Service, private ref: ChangeDetectorRef) {}
+  constructor(
+    private d3Service: D3Service,
+    private ref: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
     /** Receiving an initialized simulated graph from our custom d3 service */
-    this.graph = this.d3Service.getForceDirectedGraph(this.nodes, this.links, this.options);
+    this.graph = this.d3Service.getForceDirectedGraph(
+      this.nodes,
+      this.links,
+      this.options);
 
     /** Binding change detection check on each tick
-     * This along with an onPush change detection strategy should enforce checking only when relevant!
-     * This improves scripting computation duration in a couple of tests I've made, consistently.
-     * Also, it makes sense to avoid unnecessary checks when we are dealing only with simulations data binding.
+     * This along with an onPush change detection strategy should enforce
+     *  checking only when relevant!
+     * This improves scripting computation duration in a couple of tests
+     *  I've made, consistently.
+     * Also, it makes sense to avoid unnecessary checks when we are
+     *  dealing only with simulations data binding.
      */
     this.graph.ticker.subscribe((d) => {
       this.ref.markForCheck();
