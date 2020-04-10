cf9917c635b9f333ff161ae0d1fafea87fb43ed4
mkobar <mike@exlorev.com>
2020-04-10 17:40:36 -0400

    1st attempt at box for text

--- src/app/visuals/shared/node-visual/node-visual.component.ts
+++ src/app/visuals/shared/node-visual/node-visual.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Node } from '../../../d3';
 
+// box for text in SVG from:
+//   https://stackoverflow.com/a/42783381
+
 @Component({
   selector: '[nodeVisual]',
+  //<text filter="url(#solid)" x="20" y="50" font-size="50"> solid background </text>
   template: `
-    <svg:g [attr.transform]="'translate(' + node.x + ',' + node.y + ')'">
+  <svg:g [attr.transform]="'translate(' + node.x + ',' + node.y + ')'">
+      <defs>
+          <filter x="0" y="0" width="1" height="1.1" id="solid">
+              <feFlood flood-color="yellowgreen"/>
+              <feComposite in="SourceGraphic" operator="xor"/>
+          </filter>
+      </defs>
       <svg:circle
           class="node"
           [attr.fill]="node.color"
@@ -13,7 +23,8 @@ import { Node } from '../../../d3';
           [attr.r]="node.r">
       </svg:circle>
       <svg:text
-          class="node-name"
+	      class="node-name"
+	      filter="url(#solid)"
 	  [attr.y]="node.tagy"
           [attr.font-size]="node.fontSize">
         {{node.name}}
